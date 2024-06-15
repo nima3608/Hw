1cbb26d56ff2fc06dea989a182df5034a60cf4d5
Nima Fakhari <nima.fakhari@gmail.com>
2024-06-15 21:48:40 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,4 +5,13 @@ public class App {
         System.out.println("Hellooooooooooooooooooooooooooo, World!");
     }
 }
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1; // Initialisierung der Zählvariablen
+        while (i <= 5) { // Bedingung der while-Schleife
+            System.out.println("i: " + i); // Ausgabe des aktuellen Werts von i
+            i++; // Erhöhung der Zählvariablen
+        }
+    }
+}
 
